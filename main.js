b7be8977eef1a43dbdd2dff91d187b41a1a64975
yj0216 <yeojun0216@naver.com>
2022-10-11 21:09:53 +0900

    express4

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var template = require('./lib/template.js');
-var path = require('path');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression')
-var sanitizeHtml = require('sanitize-html');
+var topicRouter = require('./routes/topic');
+var indexRouter = require('./routes/index');
+var helmet = require('helmet')//보안 https로 바꾸는 모듈 거의 무조건 키는게 좋음
+app.use(helmet())
 const port = 3000
 
 app.use(express.static('public'));
 //middleware등록
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 //file 압축
-app.use(compression())
+app.use(compression());
 //method가 get일 경우에만 호출
 app.get('*', function (request, response, next) {
   fs.readdir('./data', function (error, filelist) {
@@ -21,8 +21,15 @@ app.get('*', function (request, response, next) {
     next();//다음에 호출할 미들웨어
   });
 })
+app.use('/',indexRouter);
+app.use('/topic',topicRouter);
+
+
+
 //route,routing 사용자들이 path를 사용해 들어올때 적절한 응답을 하는 것
 //app.get('/', (req, res) => {res.send('Hello World!')})
+
+/*
 app.get('/', function (request, response) {
 
   var title = 'Welcome';
@@ -33,67 +40,110 @@ app.get('/', function (request, response) {
     <h2>${title}</h2>${description}
     <img src="/images/hello.jpg" style="width:300px;display:block; margin-top:10px">
     `,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   response.send(html);
 });
 
-
-app.get('/page/:pageId', function (request, response,next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html)
-    }
-  })
-})
-
-
-
-app.get('/create', function (request, response) {
+app.get('/topic/create', function (request, response) {
 
   var title = 'WEB - create';
   var list = template.list(request.list);
   var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
+  <form action="/topic/create_process" method="post">
+  <p><input type="text" name="title" placeholder="title"></p>
+  <p>
+  <textarea name="description" placeholder="description"></textarea>
+  </p>
+  <p>
+  <input type="submit">
+  </p>
+  </form>
+  `, '');
   response.send(html)
 });
 
-
-app.post('/create_process', function (request, response) {
+app.post('/topic/create_process', function (request, response) {
   var post = request.body;
   var title = post.title;
   var description = post.description;
   fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.writeHead(302, { Location: `/?id=${title}` });
-    response.end();
+    response.redirect(`/topic/${title}`);
   });
+})
+  app.get('/topic/update/:pageId', function (request, response) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+      var title = request.params.pageId;
+      var list = template.list(request.list);
+      var html = template.HTML(title, list,
+        `
+        <form action="/topic/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+        <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+        <input type="submit">
+        </p>
+        </form>
+        `,
+        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+      );
+      response.send(html);
+      
+    });
+  });
+
+
+  app.post('/topic/update_process', function (request, response) {
+    var post = request.body;
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        response.redirect(`/topic/${title}`);
+      })
+    });
+  });
+
+  app.post('/topic/delete_process', function (request, response) {
+    var post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function (error) {
+      response.redirect('/');
+    })
+  });
+
+  app.get('/topic/:pageId', function (request, response, next) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+      if (err) {
+        next(err);
+      } else {
+        var title = request.params.pageId;
+        var sanitizedTitle = sanitizeHtml(title);
+        var sanitizedDescription = sanitizeHtml(description, {
+          allowedTags: ['h1']
+        });
+        var list = template.list(request.list);
+        var html = template.HTML(sanitizedTitle, list,
+          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+          ` <a href="/topic/create">create</a>
+            <a href="/topic/update/${sanitizedTitle}">update</a>
+            <form action="/topic/delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`
+        );
+        response.send(html);
+      }
+    });
+  })
+
   /*
   var body = '';
   request.on('data', function(data){
@@ -108,55 +158,15 @@ app.post('/create_process', function (request, response) {
         response.end();
       })
   });
-  */
-});
-
-app.get('/update/:pageId', function (request, response) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/create">create</a> <a href="/update/?id=${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
+*/
 
 
-app.post('/update_process', function (request, response) {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.redirect(`/?id=${title}`);
-    })
-  });
-});
 
 
-app.post('/delete_process', function (request, response) {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    response.redirect('/');
-  })
-});
+
+
+
+
 //404웹페이지 위에서부터 순차적으로 진행되기 때문에 제일 끝에 위치해야함
 app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!');
